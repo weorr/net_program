e99e1a93b9beb8d1d3a1c1194a260da96a8e5990
SuHa Kim <tngk79134@naver.com>
2025-04-21 00:20:55 +0900

    HW2_SUHA_KIM

---
+++ HW2_SUHA_KIM/hw2.py
@@ -0,0 +1,21 @@
+my_string = "My name is 김수하"
+
+print("문자 수:", len(my_string))
+
+print("10번 반복:", my_string * 10)
+
+print("첫 번째 문자:", my_string[0])
+
+print("처음 4문자:", my_string[:4])
+
+print("마지막 4문자:", my_string[-4:])
+
+print("거꾸로 출력:", my_string[::-1])
+
+print("첫 번째와 마지막 문자 제거:", my_string[1:-1])
+
+print("대문자 변환:", my_string.upper())
+
+print("소문자 변환:", my_string.lower())
+
+print("'a'를 'e'로 변경:", my_string.replace('a', 'e'))
